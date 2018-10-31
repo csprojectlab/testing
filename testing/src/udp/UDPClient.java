09114f64b5d2d5080e85915ddee41898faf1942c
Guest0 <Guest0@DESKTOP-HPL1B9N>
2018-10-31 17:35:29 +0530

    First Commit

---
+++ testing/src/udp/UDPClient.java
@@ -0,0 +1,26 @@
+package udp;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPClient {
+
+	public static void main(String[] args) throws Exception {
+		DatagramSocket socket = new DatagramSocket();
+		InetAddress IPAddress = InetAddress.getByName("localhost");
+		byte[] send = new byte[1024];
+		byte[] receive = new byte[1024];
+		String message = "This is the message from the client.";
+		send = message.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket (send, send.length, IPAddress, 8000);
+		System.out.println("Sending the message to the server.");
+		socket.send(sendPacket);
+		//------------------------------------------------------------
+		// Receiving the message from the client.
+		DatagramPacket receivePacket = new DatagramPacket(receive, receive.length);
+		socket.receive(receivePacket);
+		message = receivePacket.getData().toString();
+		System.out.println("Client receiving message from the server: " + message);
+	}
+}
---
+++ testing/src/udp/UDPServer.java
@@ -0,0 +1,50 @@
+package udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class UDPServer extends Thread {
+
+	public void run() {
+		DatagramSocket socket = null;
+		try {
+			socket = new DatagramSocket(8000);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		byte[] receive = new byte[1024];
+		byte[] send = new byte[1024];
+		while (true) {
+			DatagramPacket receivePacket = new DatagramPacket(receive, receive.length);
+			try {
+				socket.receive(receivePacket);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String data = new String(receivePacket.getData());
+			System.out.println("Received Data from client: " + data);
+			// --------------------------------------------------
+			// Sending the data back.
+			String sendData = "This is hello from the server";
+			send = sendData.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(send, send.length, receivePacket.getAddress(),
+					receivePacket.getPort());
+			try {
+				socket.send(sendPacket);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread server = new UDPServer();
+		server.start();
+	}
+
+}
